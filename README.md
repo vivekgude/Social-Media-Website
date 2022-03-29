6669a63f4550a370d1eda435652c268660e61a52
Vivek Gude <59313903+vivekgude@users.noreply.github.com>
2022-03-29 18:47:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Social-Media-Webapp
+**[Login Page](https://socialmediawebapp.herokuapp.com/)**
+<br/>
+
+## Overview
+
+Used
+
+- React and Node.js (to build out the site)
+- Authentication is done using JWT
+- styled components - Material UI and Materializecss to style the site
+- Nodemailer for resetting password through OTP
---
+++ middleware/requireLogin.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const {JWT_KEYWORD} = require('../config/keys')
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+module.exports = (req,res,next)=>{
+    const {authorization} = req.headers
+    if(!authorization){
+        return res.status(401).json({error:"youre not logged in"})    
+    }
+    const token = authorization.replace("Bearer ","")
+    jwt.verify(token,JWT_KEYWORD,(err,payload)=>{
+        if(err){
+            return res.status(401).json({error:"you must be logged in"})
+        }
+
+        const {id} = payload
+        User.findById(id)
+        .then(userdata=>{
+            req.user = userdata;
+            next();
+        })
+    })
+} 
